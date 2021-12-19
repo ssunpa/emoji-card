036e750e1eed1e239fa4b26d7c46246416d1d06c
ssunpa <bestpa.1130@gmail.com>
2021-12-19 17:23:26 +0900

    add card filp effect

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 import styles from './card.module.css';
 
-const Card = (props) => (
-    <section className={styles.card}>{props.content}</section>
-);
+const Card = (props) => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+    return (
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+            <button
+                onClick={handleClick}
+                className={`${styles.card} ${styles.card_front}`}
+            >
+                ❓
+            </button>
+
+            <button
+                onClick={handleClick}
+                className={`${styles.card} ${styles.card_back}`}
+            >
+                {props.content}
+            </button>
+        </ReactCardFlip>
+    );
+};
 
 export default Card;
