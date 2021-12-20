41d24cc42fecc89770a77c8e029ea957f3a9b80d
ssunpa <bestpa.1130@gmail.com>
2021-12-20 18:10:54 +0900

    add card items reset button

--- src/app.jsx
+++ src/app.jsx
@@ -2,19 +2,34 @@ import React, { useState } from 'react';
 import styles from './app.module.css';
 import CardHide from './components/bottom/card_hide';
 import CardShow from './components/top/card_show';
+import { emoji } from './data/emoji';
 
 function App() {
-    const [cardItems, setCardItems] = useState([
-        { id: 1, content: '😀' },
-        { id: 2, content: '😁' },
-        { id: 3, content: '😂' },
-        { id: 4, content: '😆' },
-        { id: 5, content: '😘' },
-        { id: 6, content: '😴' },
-    ]);
+    const [cardItems, setCardItems] = useState(emoji.slice(0, 6));
 
-    const handleRandomItem = (items) => {
-        const randomNum = Math.floor(Math.random() * cardItems.length);
+    const randomIdx = (items) => {
+        const randomIdx = new Set();
+
+        while (1) {
+            let randomNum = Math.floor(Math.random() * items.length);
+            randomIdx.add(randomNum);
+            if (randomIdx.size > 5) {
+                return randomIdx;
+            }
+        }
+        return randomIdx;
+    };
+
+    const handleRandomItems = () => {
+        const randomIdxArr = [...randomIdx(emoji)];
+        const newItems = randomIdxArr.map((idx) => {
+            return emoji[idx];
+        });
+        setCardItems(newItems);
+    };
+
+    const handleRandomShowItem = (items) => {
+        const randomNum = Math.floor(Math.random() * items.length);
         return items[randomNum];
     };
 
@@ -23,14 +38,19 @@ function App() {
             <section className={styles.top}>
                 <span className={styles.title}>👇FIND ME!👇</span>
                 <CardShow
-                    show={handleRandomItem(cardItems)}
+                    show={handleRandomShowItem(cardItems)}
                     key={cardItems.id}
                 />
             </section>
             <section className={styles.bottom}>
-                {cardItems.map((items) => (
-                    <CardHide hide={items.content} key={cardItems.id} />
-                ))}
+                <div className={styles.list}>
+                    {cardItems.map((items) => (
+                        <CardHide hide={items.content} key={cardItems.id} />
+                    ))}
+                </div>
+                <button onClick={handleRandomItems} className={styles.resetBtn}>
+                    RESET
+                </button>
             </section>
         </>
     );
